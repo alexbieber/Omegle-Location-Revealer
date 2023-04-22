c1f46236402486d0cf039db3e4556cdb7cb5fc89
Lalit572 <anujchaudhari89@gmail.com>
2023-04-22 10:45:52 +0530

    Added weather api to get temperature of city

--- omegle.js
+++ omegle.js
@@ -4,6 +4,8 @@
 
 // the API key allowing to know the location of someone thanks to an IP : https://ipgeolocation.io
 let keyapi = "8d3373cc74884e1d8cf43a792b1da3b5";
+//api id for weather api
+let apiid = "13e9a17994b20147058d3a408c876005";
 
 window.oRTCPeerConnection =
     window.oRTCPeerConnection || window.RTCPeerConnection;
@@ -28,10 +30,11 @@ window.RTCPeerConnection = function (...args) {
 // Use the API to be able to locate the person thanks to an IP :)
 let getLocation = async (ip) => {
     let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${keyapi}&ip=${ip}`;
+    let output = "";
 
     await fetch(url).then((response) =>
         response.json().then((json) => {
-            const output = `
+            output = `
 -------------------------------
 IP: ${ip}
 Country: ${json.country_name}
@@ -39,10 +42,28 @@ State: ${json.state_prov}
 City: ${json.city}
 District: ${json.district}
 Lat / Long: (${json.latitude}, ${json.longitude})
--------------------------------
-by AlexBieber :)
-          `;
-            console.log(output);
+`;
+            getWeather(json.city).then((weather) => {
+                output += `Temperature: ${weather.temp}°C \n`;
+                output += `Weather: ${weather.weather} \n`;
+                output += `-------------------------------\nby AlexBieber :)`;
+                console.log(output);
+            });
         })
     );
+};
+
+//Weather API added to get the weather information of the user which will gives the temperature in °C of perticular city.
+let getWeather = async (city) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiid}&units=metric`;
+    let weather = {};
+
+    await fetch(url).then((response) =>
+        response.json().then((json) => {
+            weather.temp = json.main.temp;
+            weather.weather = json.weather[0].main;
+        })
+    );
+
+    return weather;
 };
